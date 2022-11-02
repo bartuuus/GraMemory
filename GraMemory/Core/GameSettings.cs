5190e1e3cb7a1fd6129c45ffa84ac1f8c4a1b54a
Giganci Programowania <giganci12.visual@gmail.com>
2022-11-02 19:09:12 +0100

    Ustawienia bazowe

---
+++ GraMemory/Core/GameSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMemory.Core
+{
+   public class GameSettings
+    {
+        public int CzasGry { get; private set; }
+        public int CzasPodgladu { get; private set; }
+        public int MaxPunkty { get; private set; }
+        public int Wiersze { get; private set; }
+        public int Kolumny { get; private set; }
+        public int Bok { get; private set; }
+        public int AktualnePunkty { get; private set; }
+
+        public string PlikLogo => $"{Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "logo.jpg")}";
+        public string FolderObrazki => $"{Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "memory")}";
+        public GameSettings()
+        {
+            UstawStartowe();
+        }
+
+        public void UstawStartowe()
+        {
+            CzasPodgladu = 5;
+            CzasGry = 60;
+            MaxPunkty = 0;
+            Wiersze = 4;
+            Kolumny = 6;
+            Bok = 150;
+            AktualnePunkty = 0;
+            
+
+
+        }
+
+
+    }
+}
--- GraMemory/MemoryForm.cs
+++ GraMemory/MemoryForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GraMemory.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,15 @@ namespace GraMemory
 {
     public partial class MemoryForm : Form
     {
+        private GameSettings _settings;
+
+
         public MemoryForm()
         {
             InitializeComponent();
+            _settings = new GameSettings();
         }
 
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
